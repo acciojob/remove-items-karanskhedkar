64ed266c1a6443c8ef8d8bbdb06f222267579791
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-18 17:51:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const removeColor=()=>{
+    const select=document.getElementById("colorSelect");
+	let selectedIndex=document.getElementById("colorSelect").selectedIndex;;
+    if(selectedIndex!=-1){
+      select.remove(selectedIndex)
+    }
+}
